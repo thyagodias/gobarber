81d01361b45b12709f3e62eeb1ea9dcac77b1aad
Thyago Dias <thyago.dias@unemat.br>
2020-04-23 00:47:17 -0400

    cria rota de appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'tudo ok' });
